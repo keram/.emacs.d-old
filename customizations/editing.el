e42cd91a3642a0880cf42a4445207ec29c949659
Marek L <nospam.keram@gmail.com>
2018-01-15 21:29:41 +0000

    Use spaces for indentation

--- customizations/editing.el
+++ customizations/editing.el
@@ -28,8 +28,8 @@
 ;;(global-set-key (kbd "C-M-s") 'isearch-forward)
 ;;(global-set-key (kbd "C-M-r") 'isearch-backward)
 ;;
-;;;; Don't use hard tabs
-;;(setq-default indent-tabs-mode nil)
+;; Don't use hard tabs
+(setq-default indent-tabs-mode nil)
 ;;
 ;;;; When you visit a file, point goes to the last place where it
 ;;;; was when you previously visited the same file.
