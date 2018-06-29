81452b62038aecdd19693ea5f2a29574f72b4140
keram <mlabos@bookingbug.com>
2018-06-29 16:53:22 +0100

    Cleanup org org-capture setttings

--- customizations/setup-org-capture.el
+++ customizations/setup-org-capture.el
@@ -1,22 +1,20 @@
 (setq org-directory "~/docs/org")
-(setq org-default-notes-file "~/docs/org/refile.org")
-
-;; I use C-c c to start capture mode
-(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-default-notes-file (concat org-directory "/" "refile.org"))
+(setq org-log-done t)
 
 ;; Capture templates for: TODO tasks, Notes, habits, meetings, and org-protocol
 (setq org-capture-templates
-      (quote (("t" "todo" entry (file "~/docs/org/refile.org")
+      (quote (("t" "todo" entry (file org-default-notes-file)
                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("n" "note" entry (file "~/docs/org/refile.org")
+              ("n" "note" entry (file org-default-notes-file)
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/docs/org/diary.org")
+              ("j" "Journal" entry (file+datetree (concat org-directory "/" "diary.org"))
                "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/docs/org/refile.org")
+              ("w" "org-protocol" entry (file org-default-notes-file)
                "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("m" "Meeting" entry (file "~/docs/org/refile.org")
+              ("m" "Meeting" entry (file org-default-notes-file)
                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file "~/docs/org/refile.org")
+              ("h" "Habit" entry (file org-default-notes-file)
                "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
 ;; Remove empty LOGBOOK drawers on clock out
@@ -27,3 +25,10 @@
     (org-remove-empty-drawer-at "LOGBOOK" (point))))
 
 (add-hook 'org-clock-out-hook 'bh/remove-empty-drawer-on-clock-out 'append)
+
+(global-set-key (kbd "C-c o")
+                (lambda () (interactive) (find-file org-default-notes-file)))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -1,14 +1,1 @@
 (require 'org)
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
-(setq org-log-done t)
-
-(setq org-agenda-files (quote ("~/docs/org")))
-;;(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map "\C-cc" 'org-capture)
-
-(global-set-key (kbd "C-c o")
-		(lambda () (interactive) (find-file "~/docs/org/refile.org")))
-
