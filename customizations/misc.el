3bdc1ba48a4814d8805e03dbd8c209c4ce0dc8e7
keram <mlabos@bookingbug.com>
2018-06-29 12:27:40 +0100

    Remove unused packages

--- customizations/misc.el
+++ customizations/misc.el
@@ -25,7 +25,7 @@
  '(coffee-tab-width 2)
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(zoom-window yaml-mode wsd-mode tagedit rspec-mode robe projectile pcre2el paredit multiple-cursors json-mode go-mode clojure-mode-extra-font-locking cider 4clojure))
+   '(zoom-window yaml-mode wsd-mode tagedit projectile pcre2el paredit multiple-cursors json-mode go-mode clojure-mode-extra-font-locking cider))
  '(safe-local-variable-values '((encoding . utf-8))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
