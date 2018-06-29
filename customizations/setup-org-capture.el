ae8f151c55c8df5e6de737b583c05bb8781c3b43
keram <mlabos@bookingbug.com>
2018-06-29 17:22:15 +0100

    Replace iswitch with default working switchb

--- customizations/setup-org-capture.el
+++ customizations/setup-org-capture.el
@@ -31,4 +31,4 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cb" 'org-switchb)
